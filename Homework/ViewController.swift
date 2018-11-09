75e625b91163306b34c1c2540d90c61ed2a1af3f
Sreng Khorn <srengkhorn@macs-macbook-air.local>
2018-11-09 18:02:01 +0700

    Initial Commit

---
+++ Homework/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Homework
+//
+//  Created by Sreng Khorn on 9/11/18.
+//  Copyright © 2018 Sreng Khorn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
