f7770847d908cfb8e1afd3bb2bbc0b555b0335a4
Sreng Khorn <srengkhorn@Macs-MacBook-Air.local>
2018-11-10 18:57:46 +0700

    udpated

--- Homework/SignupViewController.swift
+++ Homework/SignupViewController.swift
@@ -17,13 +17,36 @@ class SignupViewController: UIViewController {
     @IBOutlet weak var usernameField: UITextField!
     @IBOutlet weak var passwordField: UITextField!
     @IBOutlet weak var confirmPasswordField: UITextField!
+    @IBOutlet weak var confirmPasswordMessage: UILabel!
+    
+    @IBOutlet weak var signupButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        confirmPasswordMessage.text = ""
     }
 
     @IBAction func addNewUser(_ sender: Any) {
         delegate?.registerNewUser(["username": usernameField.text!, "password": passwordField.text!])
         self.navigationController?.popViewController(animated: true)
+    }
+    
+    @IBAction func confirmEnd(_ sender: Any) {
+        if passwordField.text?.isEmpty ?? true || confirmPasswordField.text?.isEmpty ?? true {
+            confirmPasswordMessage.text = ""
+        }
+    }
+}
+
+
+extension SignupViewController {
+    @IBAction func confirmEditingChanged(_ sender: Any) {
+        if passwordField.text != confirmPasswordField.text {
+            confirmPasswordMessage.text = "confirm password doesn't match!"
+            signupButton.isEnabled = false
+        } else {
+            confirmPasswordMessage.text = ""
+            signupButton.isEnabled = true
+        }
     }
 }
